b0ede3d77ccce5ccbbb189895d8784137d143118
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-04-02 12:50:54 +0300

    Update model class

--- cryptoapi/models/model.py
+++ cryptoapi/models/model.py
@@ -3,8 +3,18 @@ from cerberus import Validator
 
 class Model:
 
-    def __init__(self, schema):
-        self.validator = Validator(schema)
+    def __init__(self, schema, item=False):
+        if isinstance(schema, dict):
+            schema = Validator(schema)
+        self._validator = schema
+        self._item = item
+
+    def _validate(self, data):
+        if isinstance(self._validator, Validator):
+            return self._validator.validate(data)
+        return self._validator(data)
 
     def validate(self, data):
-        return self.validator.validate(data)
+        if self._item:
+            return all([self._validate(item) for item in data])
+        return self._validate(data)
