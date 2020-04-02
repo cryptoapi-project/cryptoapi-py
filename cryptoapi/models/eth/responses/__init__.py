7dd5789a4a0f34051f934e1ba333d3ffb55de14f
y.sidoruk <y.sidoruk@pixelplex.io>
2020-04-02 14:46:38 +0300

    update init model

--- cryptoapi/models/eth/responses/__init__.py
+++ cryptoapi/models/eth/responses/__init__.py
@@ -5,7 +5,7 @@ from .schemes import get_network_information, estimate_gas, get_block_informatio
     get_token_balance_by_holders_and_token, get_transactions_with_pagination, \
     get_transaction_information, get_transaction_receipt, send_transaction, \
     decode_transaction, get_tokens, get_token_transfers_by_token_address, get_token_contract, \
-    subscribe_to_addresses_notifications, unsubscribe_from_addresses_notifications, get_contracts_logs, \
+    subscribe_to_addresses_notifications, get_contracts_logs, \
     contract_call, get_contract_general_information
 
 
@@ -20,8 +20,8 @@ class Responses:
             get_information_about_the_latest_blocks_with_pagination)
         self.get_transactions_by_addresses = model_wrapper(get_transactions_by_addresses)
         self.get_transaction_intersections_by_addresses = model_wrapper(get_transaction_intersections_by_addresses)
-        self.get_balances_by_addresses = model_wrapper(get_balances_by_addresses)
-        self.get_general_information_by_addresses = model_wrapper(get_general_information_by_addresses)
+        self.get_balances_by_addresses = model_wrapper(get_balances_by_addresses, True)
+        self.get_general_information_by_addresses = model_wrapper(get_general_information_by_addresses, True)
         self.get_token_transfers_by_addresses = model_wrapper(get_token_transfers_by_addresses)
         self.get_tokens_balances_by_holders = model_wrapper(get_tokens_balances_by_holders)
         self.get_token_balance_by_holders_and_token = model_wrapper(get_token_balance_by_holders_and_token)
@@ -34,7 +34,6 @@ class Responses:
         self.get_token_transfers_by_token_address = model_wrapper(get_token_transfers_by_token_address)
         self.get_token_contract = model_wrapper(get_token_contract)
         self.subscribe_to_addresses_notifications = model_wrapper(subscribe_to_addresses_notifications)
-        self.unsubscribe_from_addresses_notifications = model_wrapper(unsubscribe_from_addresses_notifications)
-        self.get_contracts_logs = model_wrapper(get_contracts_logs)
+        self.get_contracts_logs = model_wrapper(get_contracts_logs, True)
         self.contract_call = model_wrapper(contract_call)
         self.get_contract_general_information = model_wrapper(get_contract_general_information)
