9fc3a5af693aac35100ff64e9be7bfba8358a05f
y.sidoruk <y.sidoruk@pixelplex.io>
2020-04-03 16:59:09 +0300

    update required fields

--- cryptoapi/models/eth/requests/schemes.py
+++ cryptoapi/models/eth/requests/schemes.py
@@ -1,36 +1,40 @@
-from cryptoapi.models.utils import regex_is_hex, hex_type, string_type, integer_type, required_string_type, boolean_type
+from cryptoapi.models.utils import hex_type_not_required, string_type_not_required, integer_type_not_required,\
+    required_string_type, boolean_type_not_required
 
 
 # ETH.Common
 estimate_gas = {
-    'from': hex_type,
-    'to': hex_type,
-    'data': hex_type,
-    'value': hex_type
+    'from': hex_type_not_required,
+    'to': hex_type_not_required,
+    'data': hex_type_not_required,
+    'value': hex_type_not_required
 }
 
 # ETH.Blocks
 get_block_information_by_block_number_or_hash = {
-    'block_number_or_hash': {'type': ['string', 'integer']}
+    'block_number_or_hash': {
+        'type': ['string', 'integer'],
+        'required': True
+    }
 }
 
 get_information_about_the_latest_blocks_with_pagination = {
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
 # ETH.Addresses
 get_transactions_by_addresses = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type,
-    'positive': string_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
+    'positive': string_type_not_required
 }
 
 get_transaction_intersections_by_addresses = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
 get_balances_by_addresses = {
@@ -41,81 +45,75 @@ get_general_information_by_addresses = {
 }
 
 get_token_transfers_by_addresses = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'required': True,
         'type': 'list',
-        'schema': string_type},
+        'schema': required_string_type
+    },
     'token': required_string_type
 }
 
 get_tokens_balances_by_holders = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
 get_token_balance_by_holders_and_token = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'required': True,
         'type': 'list',
-        'schema': string_type},
+        'schema': required_string_type
+    },
     'token': required_string_type
 }
 
 # ETH.Transactions
 get_transactions_with_pagination = {
-    'from': hex_type,
-    'to': hex_type,
-    'skip': integer_type,
-    'limit': integer_type,
-    'block_number': integer_type
+    'from': hex_type_not_required,
+    'to': hex_type_not_required,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
+    'block_number': integer_type_not_required
 }
 
 get_transaction_information = {
-    'hash': {
-        'required': True,
-        'type': 'string',
-        'regex': regex_is_hex
-    }
+    'hash': hex_type_not_required
 }
 
 get_transaction_receipt = {
-    'hash': {
-        'required': True,
-        'type': 'string',
-        'regex': regex_is_hex
-    }
+    'hash': hex_type_not_required
 }
 
 send_transaction = {
-    'tx': hex_type
+    'tx': hex_type_not_required
 }
 
 decode_transaction = {
-    'tx': hex_type
+    'tx': hex_type_not_required
 }
 
 # ETH.Tokens
 get_tokens = {
-    'query': string_type,
-    'skip': integer_type,
-    'limit': integer_type,
+    'query': string_type_not_required,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'types': {
         'type': 'list',
-        'schema': string_type
+        'schema': string_type_not_required
     }
 }
 
 get_token_transfers_by_token_address = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'type': 'list',
-        'schema': string_type
+        'schema': string_type_not_required
     },
     'token': required_string_type
 }
@@ -124,7 +122,7 @@ get_token_contract = {
     'address': {
         'required': True,
         'type': 'list',
-        'schema': string_type
+        'schema': required_string_type
     },
 }
 
@@ -144,17 +142,17 @@ unsubscribe_from_addresses_notifications = {
 
 # ETH.Contracts
 get_contracts_logs = {
-    'cursor': string_type,
-    'reversed_fetch': boolean_type,
-    'from_block': integer_type,
-    'to_block': integer_type,
+    'cursor': string_type_not_required,
+    'reversed_fetch': boolean_type_not_required,
+    'from_block': integer_type_not_required,
+    'to_block': integer_type_not_required,
     'addresses': {
         'type': 'list',
-        'schema': string_type
+        'schema': string_type_not_required
     },
     'topics': {
         'type': 'list',
-        'schema': string_type
+        'schema': string_type_not_required
     }
 }
 
--- cryptoapi/models/klay/requests/schemes.py
+++ cryptoapi/models/klay/requests/schemes.py
@@ -1,36 +1,39 @@
-from cryptoapi.models.utils import regex_is_hex, hex_type, string_type, integer_type, required_string_type, boolean_type
+from cryptoapi.models.utils import hex_type_not_required, string_type_not_required, integer_type_not_required, required_string_type, boolean_type_not_required
 
 
-# ETH.Common
+# KLAY.Common
 estimate_gas = {
-    'from': hex_type,
-    'to': hex_type,
-    'data': hex_type,
-    'value': hex_type
+    'from': hex_type_not_required,
+    'to': hex_type_not_required,
+    'data': hex_type_not_required,
+    'value': hex_type_not_required
 }
 
-# ETH.Blocks
+# KLAY.Blocks
 get_block_information_by_block_number_or_hash = {
-    'block_number_or_hash': {'type': ['string', 'integer']}
+    'block_number_or_hash': {
+        'type': ['string', 'integer'],
+        'required': True
+    },
 }
 
 get_information_about_the_latest_blocks_with_pagination = {
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
-# ETH.Addresses
+# KLAY.Addresses
 get_transactions_by_addresses = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type,
-    'positive': string_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
+    'positive': string_type_not_required
 }
 
 get_transaction_intersections_by_addresses = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
 get_balances_by_addresses = {
@@ -41,38 +44,38 @@ get_general_information_by_addresses = {
 }
 
 get_token_transfers_by_addresses = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'required': True,
         'type': 'list',
-        'schema': string_type},
+        'schema': required_string_type},
     'token': required_string_type
 }
 
 get_tokens_balances_by_holders = {
     'addresses': required_string_type,
-    'skip': integer_type,
-    'limit': integer_type
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required
 }
 
 get_token_balance_by_holders_and_token = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'required': True,
         'type': 'list',
-        'schema': string_type},
+        'schema': required_string_type},
     'token': required_string_type
 }
 
-# ETH.Transactions
+# KLAY.Transactions
 get_transactions_with_pagination = {
-    'from': string_type,
-    'to': string_type,
-    'skip': integer_type,
-    'limit': integer_type,
-    'block_number': integer_type
+    'from': string_type_not_required,
+    'to': string_type_not_required,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
+    'block_number': integer_type_not_required
 }
 
 get_transaction_information = {
@@ -91,23 +94,25 @@ decode_transaction = {
     'tx': required_string_type
 }
 
-# ETH.Tokens
+# KLAY.Tokens
 get_tokens = {
-    'query': string_type,
-    'skip': integer_type,
-    'limit': integer_type,
+    'query': string_type_not_required,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'types': {
         'type': 'list',
-        'schema': string_type
+        'required': False,
+        'schema': required_string_type
     }
 }
 
 get_token_transfers_by_token_address = {
-    'skip': integer_type,
-    'limit': integer_type,
+    'skip': integer_type_not_required,
+    'limit': integer_type_not_required,
     'addresses': {
         'type': 'list',
-        'schema': string_type
+        'required': False,
+        'schema': required_string_type
     },
     'token': required_string_type
 }
@@ -130,19 +135,21 @@ unsubscribe_from_addresses_notifications = {
     'firebase_token': required_string_type
 }
 
-# ETH.Contracts
+# KLAY.Contracts
 get_contracts_logs = {
-    'cursor': string_type,
-    'reversed_fetch': boolean_type,
-    'from_block': integer_type,
-    'to_block': integer_type,
+    'cursor': string_type_not_required,
+    'reversed_fetch': boolean_type_not_required,
+    'from_block': integer_type_not_required,
+    'to_block': integer_type_not_required,
     'addresses': {
         'type': 'list',
-        'schema': string_type
+        'required': False,
+        'schema': required_string_type
     },
     'topics': {
         'type': 'list',
-        'schema': string_type
+        'required': False,
+        'schema': required_string_type
     }
 }
 
