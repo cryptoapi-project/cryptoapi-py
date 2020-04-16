965fcc5131a0523feea400157589f74daf915c6e
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-04-16 12:09:04 +0300

    Add bch events

--- cryptoapi/events/__init__.py
+++ cryptoapi/events/__init__.py
@@ -1,4 +1,7 @@
 from .eth import Eth
+from .klay import Klay
+from .btc import Btc
+from .bch import Bch
 
 
 class Events:
@@ -10,6 +13,24 @@ class Events:
             api_key
     ):
         self.eth = Eth(
+            ws_wrapper,
+            config,
+            api_key,
+            debug
+        )
+        self.klay = Klay(
+            ws_wrapper,
+            config,
+            api_key,
+            debug
+        )
+        self.btc = Btc(
+            ws_wrapper,
+            config,
+            api_key,
+            debug
+        )
+        self.bch = Bch(
             ws_wrapper,
             config,
             api_key,
