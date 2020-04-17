67650e8b4f8e854606781196728a95b8075c2823
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-04-17 14:25:27 +0300

    Remove unnecessary files

--- tests/api/__init__.py
+++
@@ -1,1 +0,0 @@
-from .eth import *
--- tests/api/eth/__init__.py
+++
@@ -1,7 +0,0 @@
-from .addresses import AddressesTestCase
-from .blocks import BlocksTestCase
-from .common import CommonTestCase
-from .contracts import ContractsTestCase
-# from .push_notifications import PushNotificationTestCase
-from .tokens import TokensTestCase
-from .transactions import TransactionsTestCase
