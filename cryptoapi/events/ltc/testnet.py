3a0299435837f926e8f1a8800fb5d2ed299fdf55
y.sidoruk <y.sidoruk@pixelplex.io>
2020-05-06 16:23:29 +0300

    update events models

--- cryptoapi/events/ltc/testnet.py
+++ cryptoapi/events/ltc/testnet.py
@@ -1,9 +1,13 @@
+from cryptoapi.utils.api import validate_data
+
+
 class Testnet:
 
     def __init__(
         self,
         ws_wrapper,
         config,
+        models,
         api_key,
         debug
     ):
@@ -13,6 +17,7 @@ class Testnet:
             api_key=api_key,
             debug=debug
         )
+        self._models = models
 
     @property
     def connected(self):
@@ -25,34 +30,70 @@ class Testnet:
         self._ws.disconnect()
 
     def on_block(self, callback, confirmations=0):
+        validate_data(
+            self._models.is_integer,
+            confirmations
+        )
         return self._ws.on_event(
             ['new_block', confirmations],
             callback
         )
 
     def on_address_transactions(self, address, callback, confirmations=0):
+        validate_data(
+            self._models.is_integer,
+            confirmations
+        )
+        validate_data(
+            self._models.is_string,
+            address
+        )
         return self._ws.on_event(
             ['new_transaction', address, confirmations],
             callback
         )
 
     def on_address_balance(self, address, callback, confirmations=0):
+        validate_data(
+            self._models.is_integer,
+            confirmations
+        )
+        validate_data(
+            self._models.is_string,
+            address
+        )
         return self._ws.on_event(
             ['new_balance', address, confirmations],
             callback
         )
 
     def on_transaction_confirmations(self, _hash, confirmations, callback):
+        validate_data(
+            self._models.is_integer,
+            confirmations
+        )
+        validate_data(
+            self._models.is_string,
+            _hash
+        )
         return self._ws.on_event(
             ['new_confirmation', _hash, confirmations],
             callback
         )
 
     def on_connected(self, callback):
+        if not callable(callback):
+            raise Exception('Callback must be callable object')
         self._ws.on_connected_callbacks.append(callback)
 
     def on_disconnected(self, callback):
+        if not callable(callback):
+            raise Exception('Callback must be callable object')
         self._ws.on_disconnected_callbacks.append(callback)
 
     def unsubscribe(self, subscription_id):
+        validate_data(
+            self._models.is_integer,
+            subscription_id
+        )
         return self._ws.unsubscribe(subscription_id)
