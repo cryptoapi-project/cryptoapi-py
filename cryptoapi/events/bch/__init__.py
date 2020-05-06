04971151b7a877479abbc8b8aed53dd9b1a6b25d
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-05-06 11:20:02 +0300

    Update exists networks events

--- cryptoapi/events/bch/__init__.py
+++ cryptoapi/events/bch/__init__.py
@@ -1,4 +1,7 @@
-class Bch:
+from .testnet import Testnet
+
+
+class Bch(Testnet):
 
     def __init__(
         self,
@@ -14,45 +17,9 @@ class Bch:
             debug=debug
         )
 
-    @property
-    def connected(self):
-        return self._ws._connected
-
-    def connect(self):
-        self._ws.connect()
-
-    def disconnect(self):
-        self._ws.disconnect()
-
-    def on_block(self, callback, confirmations=0):
-        return self._ws.on_event(
-            ['new_block', confirmations],
-            callback
+        self.testnet = Testnet(
+            ws_wrapper,
+            config,
+            debug,
+            api_key
         )
-
-    def on_address_transactions(self, address, callback, confirmations=0):
-        return self._ws.on_event(
-            ['new_transaction', address, confirmations],
-            callback
-        )
-
-    def on_address_balance(self, address, callback, confirmations=0):
-        return self._ws.on_event(
-            ['new_balance', address, confirmations],
-            callback
-        )
-
-    def on_transaction_confirmations(self, _hash, confirmations, callback):
-        return self._ws.on_event(
-            ['new_confirmation', _hash, confirmations],
-            callback
-        )
-
-    def on_connected(self, callback):
-        self._ws.on_connected_callbacks.append(callback)
-
-    def on_disconnected(self, callback):
-        self._ws.on_disconnected_callbacks.append(callback)
-
-    def unsubscribe(self, subscription_id):
-        return self._ws.unsubscribe(subscription_id)
