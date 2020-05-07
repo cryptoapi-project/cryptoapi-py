e5dda570700cd97de59b7ca24ed6f1c70886c0b7
Yaroslav Sidoruk <y.sidoruk@pixelplex.io>
2020-05-07 13:50:40 +0300

    [CAPI-836] Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.1.0"
+packages = find_packages(exclude=["tests"])
+
+readme = open("README.md").read()
+long_description = readme[readme.find("Cryptoapi-py library can be used"): readme.rfind("To see `Api's`")]
+
+
+setup(
+    name='cryptoapi-py',
+    version=VERSION,
+    description="Python library for CryptoAPI.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    download_url="https://pypi.python.org/pypi/cryptoapi-py",
+    license='MIT',
+    author="PixelPlex inc",
+    author_email="dev@pixelplex.io",
+    url="https://apikey.io/",
+    keywords=["blockchain", "api", "rpc"],
+    packages=packages,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=open("requirements.txt").readlines(),
+    include_package_data=True,
+)
