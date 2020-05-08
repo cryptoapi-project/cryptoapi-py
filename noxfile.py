199cf0a986a11940cb36b670f9b8c8f0c79a41e8
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-05-08 14:50:38 +0300

    [CAPI-895] Change noxfile sessions order

--- noxfile.py
+++ noxfile.py
@@ -6,22 +6,6 @@ python_version = os.environ.get('PYTHON_VERSION', False)
 files = ['cryptoapi', 'tests', 'noxfile.py', 'setup.py']
 
 
-@nox.session(python=python_version, name='tests')
-def tests(session):
-    """Run tests."""
-    if python_version:
-        session.run('pip', 'install', '--requirement', 'requirements_dev.txt')
-    session.run('python', '-m', 'unittest', '--verbose')
-
-
-@nox.session(python=python_version, name='flake8')
-def flake8(session):
-    """Run flake8 linter."""
-    if python_version:
-        session.run('pip', 'install', '--requirement', 'requirements_dev.txt')
-    session.run('flake8', *files)
-
-
 @nox.session(python=python_version, name='isort')
 def isort(session):
     """Run isort import sorter."""
@@ -44,3 +28,19 @@ def yapf(session):
         command[-1] = '-d'
 
     session.run(*(command + files))
+
+
+@nox.session(python=python_version, name='flake8')
+def flake8(session):
+    """Run flake8 linter."""
+    if python_version:
+        session.run('pip', 'install', '--requirement', 'requirements_dev.txt')
+    session.run('flake8', *files)
+
+
+@nox.session(python=python_version, name='tests')
+def tests(session):
+    """Run tests."""
+    if python_version:
+        session.run('pip', 'install', '--requirement', 'requirements_dev.txt')
+    session.run('python', '-m', 'unittest', '--verbose')
