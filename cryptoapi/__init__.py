b1ac95793c187971ec289ebbb640193195cc8ad8
Evgenii Vasilyev <e.vasilev@pixelplex.io>
2020-05-19 15:27:44 +0300

    Add static typing to global init, configs and utils

--- cryptoapi/__init__.py
+++ cryptoapi/__init__.py
@@ -8,9 +8,9 @@ from .validators import Validators
 
 class Client:
 
-    def __init__(self, api_key, debug=False):
-        self.config = Config()
-        self.utils = Utils()
-        self.validators = Validators(self.utils)
-        self.api = Api(Http, self.config, self.validators, self.utils, api_key, debug)
-        self.events = Events(WS, self.config, self.validators, self.utils, api_key, debug)
+    def __init__(self, api_key: str, debug: bool = False) -> None:
+        self.config: Config = Config()
+        self.utils: Utils = Utils()
+        self.validators: Validators = Validators(self.utils)
+        self.api: Api = Api(Http, self.config, self.validators, self.utils, api_key, debug)
+        self.events: Events = Events(WS, self.config, self.validators, self.utils, api_key, debug)
